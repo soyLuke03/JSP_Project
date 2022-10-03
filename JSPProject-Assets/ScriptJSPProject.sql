1500bcd5b31d790b307483fe0fc8d9025e65bdca
Luke <99337120+soyLuke03@users.noreply.github.com>
2022-10-03 08:47:32 +0200

    Script modificado

--- JSPProject-Assets/ScriptJSPProject.sql
+++ JSPProject-Assets/ScriptJSPProject.sql
@@ -1,56 +1,39 @@
 Create table Users(
-	name varchar(30),
-	userName varchar(30),
-	passWord varchar(30),
-	address varchar(30),
+	name varchar(30) not null,
+	userName varchar(30) not null,
+	passWord varchar(30) not null,
+	address varchar(30) not null,
 	phoneNumber varchar(9),
-	email varchar(30),
-	id varchar(9),
+	email varchar(30) not null,
+	id varchar(9) not null,
 	
 	constraint PK_UserName primary key (userName)
 );
 
-
-Create table Admins(
-	userName varchar(30),
-	
-	constraint PK_UserName primary key (userName),
-	constraint FK_UserName foreign key (userName) references Users(userName) on delete cascade
-);
-
-
-insert into Users values ('Antonio Gonzalez Luque','IamLuke','QwErTy_03',null,null,'luquelectronico13@gmail.com','001');
-insert into Users values ('Usuario De Pruebas','DummyUser','DummyUser_00',null,null,'DummyUser@gmail.com','000');
-
-insert into Admins values ('IamLuke');
+insert into Users values ('Antonio Gonzalez Luque','IamLuke','QwErTy_03','Micasa',null,'luquelectronico13@gmail.com','001');
+insert into Users values ('Usuario De Pruebas','DummyUser','DummyUser_00','sucasa',null,'DummyUser@gmail.com','000');
 
 
 
-create table Shop(
-	idItem int(5),
-	nameItem varchar(30),
-	priceItem decimal(10,2),
-	stockItem int(5),
+
+
+create table Item(
+	idItem int(5) not null auto_increment,
+	nameItem varchar(30) not null,
+	priceItem decimal(10,2) not null,
+	stockItem int(5) not null,
+	category varchar(30),
 	
 	constraint PK_IdItem primary key (idItem)
 );
 
-
-create table Sells(
-	idItem int(5),
-	userName varchar(30),
-	
-	constraint PK_IdItemSells primary key (idItem, userName),
-	constraint FK_UserNameSells foreign key (userName) references Users(userName) on delete cascade,
-	constraint FK_IdItemSells foreign key (idItem) references Shop(idItem) on delete cascade
-);
-
-insert into Shop values (1,'Water bottle',0.80,20000);
-insert into Shop values (2,'Water bottle 1L',2.500,20000);
-insert into Shop values (3,'Uranium',2799.99,5);
-insert into Shop values (4,'Laptop Gaming',1400.50,567);
-insert into Shop values (5,'Washing machine',479.99,230);
-insert into Shop values (6,'Private Island',105000,3);
+insert into Item values (1,'Water bottle',0.80,20000, 'Drinks');
+insert into Item values (2,'Water bottle 1L',2.500,20000, 'Drinks');
+insert into Item values (3,'Uranium',2799.99,5, 'Materials');
+insert into Item values (4,'Laptop Gaming',1400.50,567,'Energy');
+insert into Item values (5,'Washing machine',479.99,230,'Appliances');
+insert into Item values (6,'Private Island',105000,3,null);
+insert into Item values (7,'Country of US',10500000,3,null);
 
 
 
