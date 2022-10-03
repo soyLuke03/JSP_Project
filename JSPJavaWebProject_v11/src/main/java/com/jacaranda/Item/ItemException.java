83294b8bb4245272d36ef4c36391f9d124472aca
Luke <99337120+soyLuke03@users.noreply.github.com>
2022-10-03 09:54:57 +0200

    CssRegister, ItemRegister, BDD, UserAccess
    Esas clases fueron modificadas

---
+++ JSPJavaWebProject_v11/src/main/java/com/jacaranda/Item/ItemException.java
@@ -0,0 +1,30 @@
+package com.jacaranda.Item;
+
+public class ItemException extends RuntimeException {
+
+	public ItemException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
