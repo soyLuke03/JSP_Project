9d02c69e5f2e5ccdcd5628e2f77293bd80fc9131
Luke <99337120+soyLuke03@users.noreply.github.com>
2022-10-19 08:21:42 +0200

    Delete JSPJavaWebProject_v11 directory

--- JSPJavaWebProject_v11/src/main/java/com/jacaranda/Item/Category.java
+++
@@ -1,9 +0,0 @@
-package com.jacaranda.Item;
-
-public enum Category {
-
-	Food, Drinks, Furniture, Materials,
-	Transport, Machinery, Energy, Appliances,
-	Other;
-	
-}
--- JSPJavaWebProject_v11/src/main/webapp/jsScripts/script.js
+++
@@ -1,6 +0,0 @@
-function ver(n) {
-    document.getElementById("subseccion"+n).style.display="block"
-    }
-function ocultar(n) {
-    document.getElementById("subseccion"+n).style.display="none"
-    }
